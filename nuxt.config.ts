07ed4d5b2599f2ed64e0a492c27ea14ba97c648b
SavioHaistem <savio.dantas.5858@gmail.com>
2023-09-21 15:41:29 -0300

    fix: try fix login errors nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["@/assets/scss/CollorPallete.scss"],
   components: [{path: '@/components', pathPrefix: false}],
+  modules: ['@pinia/nuxt'],
+  imports: {
+    autoImport: true,
+    global: true,
+  }
 })
