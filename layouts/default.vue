47b2edafc9399e1cd3fc308ca6d320378c66f250
Gabriel <gabrielmesquita0503@gmail.com>
2023-09-21 15:15:44 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+        <slot />
+
+    </div>
+</template>
+
+<script lang="ts">
+import '@appwrite.io/pink';
+import '@appwrite.io/pink-icons';
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ["@/assets/scss/CollorPallete.scss"],
+  components: [{path: '@/components', pathPrefix: false}],
+})
