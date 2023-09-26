80b1cabc778000a8213cd73e8b20d2f0ff6b7b41
Gabriel <gabrielmesquita0503@gmail.com>
2023-09-26 14:49:43 -0300

    Fix: unnecessary import

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,7 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  import LoginForm from '~/components/molecules/LoginForm.vue'
-  //const route = useRouter();
 
-  export default defineComponent({
-    components: {
-      LoginForm
-    }
-  })
 </script>
 <style lang="css">
   div.login-page {
