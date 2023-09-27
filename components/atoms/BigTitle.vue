e703f147d92b780d9f39f7c3b563e8f3d5e5d577
SavioHaistem <savio.dantas.5858@gmail.com>
2023-09-27 19:42:26 -0300

    feat: titulo da tela inicial

--- components/atoms/BigTitle.vue
+++ components/atoms/BigTitle.vue
@@ -1,12 +1,24 @@
 <template>
-   <div class="title-area">
-      <p class="title">Cred.io</p>
-   </div>
+  <div class="title-area">
+    <p class="title">Cred.io</p>
+  </div>
 </template>
 <style scoped>
-div.title-area {
-   border: 1px solid red;
-   height: 20rem;
-   width: 20rem;
-}
+  div.title-area {
+    background-color: #159947;
+    background-image: url('../../public/imgs/Group_9.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    border-radius: 0px 0px 0px 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  div.title-area > p.title {
+    color: white;
+    font-size: 65px;
+    font-family: 'Poppins';
+    font-weight: 700;
+  }
 </style>
